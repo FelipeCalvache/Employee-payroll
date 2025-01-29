331bb0d56ea07574961e66f98b6e2274dc3a8c70
FelipeCalvache <felipecalvache6@gmail.com>
2025-01-29 07:34:14 -0500

    add WorkDay class

--- src/main/java/com/mgmingenieriayproyectos/nomina_empleados/model/WorkDay.java
+++ src/main/java/com/mgmingenieriayproyectos/nomina_empleados/model/WorkDay.java
@@ -3,6 +3,7 @@ package com.mgmingenieriayproyectos.nomina_empleados.model;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 
 public class WorkDay {
     @Id
@@ -10,4 +11,43 @@ public class WorkDay {
     private Long id;
     private BiometricRecord start;
     private BiometricRecord end;
+    @ManyToOne
+    private Employee employee;
+
+    public Long getId() {
+        return id;
+    }
+
+    public BiometricRecord getStart() {
+        return start;
+    }
+
+    public void setStart(BiometricRecord start) {
+        this.start = start;
+    }
+
+    public BiometricRecord getEnd() {
+        return end;
+    }
+
+    public void setEnd(BiometricRecord end) {
+        this.end = end;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public WorkDay(BiometricRecord start, BiometricRecord end, Employee employee) {
+        this.start = start;
+        this.end = end;
+        this.employee = employee;
+    }
+
+    public WorkDay() {
+    }
 }
